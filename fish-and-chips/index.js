813069c025da29d37bc11a7412277c309cc579ce
Karan Kamath <karankamath26@gmail.com>
2014-10-06 01:12:39 +0800

    Add core functionality

---
+++ fish-and-chips/index.js
@@ -0,0 +1,12 @@
+/**
+ * Replace British English with American English
+ *
+ * @param {String} doc
+ * @return {String}
+ */
+module.exports = {
+    toAmerican: function(doc) {
+        return String(dox)
+            .replace('centre', 'center')
+            .replace('colour', 'color')
+    }
